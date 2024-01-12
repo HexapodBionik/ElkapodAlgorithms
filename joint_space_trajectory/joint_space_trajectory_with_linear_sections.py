e6537fed21ca4f3e9d8ca70311defd2e50901264
Piotr Patek <piotr.patek.stud@pw.edu.pl>
2024-01-13 00:20:56 +0100

    #1 Implemented LSPB trajectory solver for two point, other minor changes

--- joint_space_trajectory/joint_space_trajectory_with_linear_sections.py
+++ joint_space_trajectory/joint_space_trajectory_with_linear_sections.py
@@ -1,9 +1,8 @@
 from kinematics.kinematics import KinematicsSolver
-from joint_trajectories import ViaPointsJointSpaceTrajectory
+from joint_trajectories import LSPBTwoPoints, SimpleJointSpaceTrajectory
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-from matplotlib.animation import FuncAnimation
 matplotlib.use('Qt5Agg')
 
 # Wszystkie wartości translacji w metrach
@@ -21,27 +20,27 @@ t3 = np.array([0.1, 0, 0, 1])
 
 
 kinematics_solver = KinematicsSolver(mount_t, t1, t2, t3, 0, -np.pi/2)
-via_points_trajectory = ViaPointsJointSpaceTrajectory(kinematics_solver)
+linear_trajectory = LSPBTwoPoints(kinematics_solver)
 
-# 5 interpolation points (2 boundary points and 3 via points), each represented as subarray with values in x, y and z axis
-coordinates = np.array([[0, -0.05, 0], [0.05, -0.02, 0.05], [0.05, 0, 0.06], [0.05, 0.02, 0.05], [0, 0.05, 0]])
+start_coordinates = np.array([0.01, -0.02, -0.15])
+end_coordinates = np.array([0.01, 0.02, -0.135])
 
-# Boundary velocities (start and finish velocity) for each joint
-velocities = np.array([[0,0], [0,0], [0,0]])
-
-# Move times meaning time for each joint to move from currently starting point to the next point
-move_times = np.array([1.5, 0.25, 0.25, 1.5])
+velocity_start = np.array([0, 0, 0])
+velocity_end = np.array([0, 0, 0])
+time_end = 3
+max_acceleration = np.array([4, 0.1, 4]) # rads/s^2
 
 # Time steps for whole movement probed with f=200Hz (T=5ms)
-x = np.arange(0, sum(move_times), 0.005)
+x = np.arange(0, time_end, 0.005)
 
 
-polys = via_points_trajectory.generate_trajectory(coordinates, velocities, move_times)
+linear_trajectory_polynomials = linear_trajectory.generate_trajectory(start_coordinates, end_coordinates, velocity_start, velocity_end, max_acceleration, time_end)
 
 
-midtrajectory_positions = polys[0](x)
-midtrajectory_vels = polys[0].get_velocity(x)
-midtrajectory_accels = polys[0].get_acceleration(x)
+joint = 1
+midtrajectory_positions = linear_trajectory_polynomials[joint](x)
+midtrajectory_vels = linear_trajectory_polynomials[joint].get_velocity(x)
+midtrajectory_accels = linear_trajectory_polynomials[joint].get_acceleration(x)
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
 
@@ -69,13 +68,14 @@ ax3.grid()
 # Adjust layout to prevent overlap
 plt.tight_layout()
 
+
 x_points = []
 y_points = []
 z_points = []
 
-theta1 = polys[0](x)
-theta2 = polys[1](x)
-theta3 = polys[2](x)
+theta1 = linear_trajectory_polynomials[0](x)
+theta2 = linear_trajectory_polynomials[1](x)
+theta3 = linear_trajectory_polynomials[2](x)
 for i in range(len(x)):
     forward_coordinates = kinematics_solver.forward(theta1[i], theta2[i], theta3[i])
     x_points.append(forward_coordinates[0])
@@ -88,10 +88,11 @@ ax = fig.add_subplot(111, projection='3d')
 ax.set_title(label="Trajectory with 2 via points generated with spline polynomial")
 scatter = ax.scatter(x_points, y_points, z_points, marker='o', label="Spline polynomial")
 ax.view_init(elev=45, azim=135)
-coordinate_points_x = [x[0] for x in coordinates]
-coordinate_points_y = [x[1] for x in coordinates]
-coordinate_points_z = [x[2] for x in coordinates]
-point = ax.scatter(coordinate_points_x, coordinate_points_y, coordinate_points_z, color='red', s=100, label="Interpolated points")
+
+coordinate_points_x = [start_coordinates[0], end_coordinates[0]]
+coordinate_points_y = [start_coordinates[1], end_coordinates[1]]
+coordinate_points_z = [start_coordinates[2], end_coordinates[2]]
+ax.scatter(coordinate_points_x, coordinate_points_y, coordinate_points_z, color='red', s=100, label="Interpolated points")
 
 # Add labels
 ax.set_xlabel('X Axis [m]')
@@ -99,21 +100,5 @@ ax.set_ylabel('Y Axis [m]')
 ax.set_zlabel('Z Axis [m]')
 ax.legend()
 
-
 # Show the interactive plot
-def animate(i):
-    theta1, theta2, theta3 = [float(polys[x](np.array([i * 0.005]))) for x in range(3)]
-    forward_coordinates = kinematics_solver.forward(theta1, theta2, theta3)
-
-    # Update the 3D scatter plot directly
-    point._offsets3d = ([forward_coordinates[0]], [forward_coordinates[1]],
-                        [forward_coordinates[2]])
-
-    return point,
-
-
-ani = FuncAnimation(fig, animate, interval=len(x))
-
-
 plt.show()
-
--- joint_space_trajectory/utils.py
+++ joint_space_trajectory/utils.py
@@ -8,3 +8,66 @@ def acquire_trajectory_functions(poly_coefficients: np.ndarray) \
     velocity_polynomial = np.polyder(position_polynomial)
     acceleration_polynomial = np.polyder(velocity_polynomial)
     return position_polynomial, velocity_polynomial, acceleration_polynomial
+
+
+class SplinePolynomial:
+    def __init__(self, coefficients_list: np.ndarray, poly_periods: np.ndarray,
+                 poly_start_times=None):
+        self._coefficients_list = coefficients_list
+        self._poly_periods = poly_periods
+        self._time_limits = [sum(self._poly_periods[:i]) for i in
+                             range(1,
+                                   len(self._poly_periods) + 1)]
+
+        if poly_start_times is None:
+            self._poly_start_times = self._time_limits
+        else:
+            self._poly_start_times = poly_start_times
+
+    def __call__(self, time_steps: np.ndarray) -> float:
+        values = []
+        for time_step in time_steps:
+            poly, polynumber = self._select_polynomial(time_step)
+
+            values.append(
+                self._generate_polynomial_value(poly, time_step, polynumber))
+
+        return np.array(values)
+
+    def _select_polynomial(self, time_step: float) -> Tuple[
+                                                          np.poly1d, int] | None:
+        for i, time_limit in enumerate(self._time_limits):
+            if time_step < time_limit:
+                polynumber = i
+
+                poly = np.poly1d(np.flip(self._coefficients_list[polynumber]))
+                return poly, polynumber
+        return None
+
+    def _generate_polynomial_value(self, poly: np.poly1d, time_step: float,
+                                   polynumber: int) -> float:
+        if polynumber == 0:
+            return poly(time_step)
+        else:
+            return poly(
+                time_step - self._poly_start_times[polynumber - 1])
+
+    def get_velocity(self, time_steps: np.ndarray) -> float:
+        values = []
+        for time_step in time_steps:
+            poly, polynumber = self._select_polynomial(time_step)
+            poly = np.polyder(poly)
+
+            values.append(
+                self._generate_polynomial_value(poly, time_step, polynumber))
+        return np.array(values)
+
+    def get_acceleration(self, time_steps: np.ndarray) -> float:
+        values = []
+        for time_step in time_steps:
+            poly, polynumber = self._select_polynomial(time_step)
+            poly = np.polyder(poly, 2)
+
+            values.append(
+                self._generate_polynomial_value(poly, time_step, polynumber))
+        return np.array(values)
