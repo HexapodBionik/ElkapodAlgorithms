95d5d35d519f45e207d25cf01fbaaa7b4c5fb172
dbarania <baraniak.damian@gmail.com>
2024-02-06 22:10:02 +0100

    Readjusted argumets and outputs for forward and inverse kinematics solvers

--- kinematics/kinematics.py
+++ kinematics/kinematics.py
@@ -72,7 +72,7 @@ def se3_norm(v):
     assert v[3] == 1
 
     return np.linalg.norm(
-            np.array([v[0], v[1], v[2]]))
+        np.array([v[0], v[1], v[2]]))
 
 
 class KinematicsSolver:
@@ -82,15 +82,12 @@ class KinematicsSolver:
     The leg has 3 degrees of freedom.
     """
 
-    def __init__(self, mount_t: np.ndarray, t1: np.ndarray, t2: np.ndarray, t3: np.ndarray,
-                 ma2: float, ma3: float):
+    def __init__(self, mount_t: np.ndarray, t1: np.ndarray, t2: np.ndarray, t3: np.ndarray):
         """
         @param mount_t: Translation from world origin to J1
         @param t1: Translation 1 (between J1 and J2)
         @param t2: Translation 2 (between J2 and J3)
         @param t3: Translation 3 (between J3 and FCP)
-        @param ma2: Servo mount angle 2 (in radians)
-        @param ma3: Servo mount angle 3 (in radians)
         """
 
         assert mount_t.ndim == 1
@@ -109,71 +106,72 @@ class KinematicsSolver:
         assert t3[3] == 1
 
         self._mount_t = mount_t
-        self.t1 = t1
-        self.t2 = t2
-        self.t3 = t3
-        self.ma2 = ma2
-        self.ma3 = ma3
+        self._t1 = t1
+        self._t2 = t2
+        self._t3 = t3
 
-    def forward(self, q1: float, q2: float, q3: float) -> np.ndarray:
+    def forward(self, q: np.ndarray) -> np.ndarray:
         """
         Forward Kinematics
 
-        @param q1: Joint1 angle (in radians)
-        @param q2: Joint2 angle (in radians)
-        @param q3: Joint3 angle (in radians)
-        @return: Position of leg's foot center point
+        @param q: Vector of joint variables [q1,q2,q3]
+        @return: Position of leg's foot center point as [x,y,z]
         """
 
-        m = trans(self._mount_t)
-        m = m.dot(rot_z(q1))
-        m = m.dot(trans(self.t1))
-        m = m.dot(rot_x(math.pi/2))
-        m = m.dot(rot_z(q2+self.ma2))
-        m = m.dot(trans(self.t2))
-        m = m.dot(rot_z(q3+self.ma3))
-        m = m.dot(trans(self.t3))
-        m = m.dot(rot_z(math.pi))
+        # m = trans(self._mount_t)
+        # m = m.dot(rot_z(q1))
+        # m = m.dot(trans(self.t1))
+        # m = m.dot(rot_x(math.pi/2))
+        # m = m.dot(rot_z(q2+self.ma2))
+        # m = m.dot(trans(self.t2))
+        # m = m.dot(rot_z(q3+self.ma3))
+        # m = m.dot(trans(self.t3))
+        # m = m.dot(rot_z(math.pi))
+        pass
+        # return m.dot(np.array([0, 0, 0, 1]))
 
-        return m.dot(np.array([0, 0, 0, 1]))
-
-    def inverse(self, v: np.ndarray) -> Tuple[float, float, float]:
+    def inverse(self, p: np.ndarray) -> np.ndarray:
         """
         Inverse Kinematics
 
-        @param v: Desired position of arm's end | (x, y, z, 1)
+        @param p: Desired position of arm's end | (x, y, z)
         @return: Angles to be set on servos
         """
 
-        assert v.ndim == 1
-        assert len(v) == 4
-        assert v[3] == 1
+        # assert p.ndim == 1
+        # assert len(p) == 3
+        #
+        # x = p[0]
+        # y = p[1]
+        # z = p[2]
+        #
+        # l1 = se3_norm(self.t1)
+        # l2 = se3_norm(self.t2)
+        # l3 = se3_norm(self.t3)
+        #
+        # q1 = (math.pi / 2) * np.sign(y) if x == 0 \
+        #     else math.atan2(y, x)
+        #
+        # p = rot_z(-q1).dot(p)
+        # x = p[0] - self.t1[0]
+        # y = p[1]
+        # z = p[2] - self._mount_t[2] - self.t1[2]
+        #
+        # q2 = (math.pi / 2) * np.sign(z) if x == 0 \
+        #     else math.atan2(z, x)
+        #
+        # q2 += np.arccos((l2 ** 2 + x ** 2 + z ** 2 - l3 ** 2) / (2 * l2 * np.sqrt(x ** 2 + z ** 2)))
+        #
+        # q3 = math.pi - np.arccos((l2 ** 2 + l3 ** 2 - x ** 2 - z ** 2) / (2 * l2 * l3))
+        x = p[0]
+        y = p[1]
+        z = p[2]
 
-        x = v[0]
-        y = v[1]
-        z = v[2]
+        q1 = np.arctan2(y, x)
+        l = np.sqrt(x ** 2 + y ** 2) - self._t1[0]
+        z = z - self._mount_t[2] - self._t1[2]
 
-        l1 = se3_norm(self.t1)
-        l2 = se3_norm(self.t2)
-        l3 = se3_norm(self.t3)
+        q2 = np.arctan2(z, l) + np.arccos((l ** 2 + self._t2[0] ** 2 - self._t3[0] ** 2) / (2 * l * self._t2[0]))
+        q3 = np.arccos((self._t2[0] ** 2 + self._t3[0] ** 2 - l ** 2) / (2 * self._t2[0] * self._t3[0])) - np.pi
 
-        q1 = (math.pi/2)*np.sign(y) if x == 0 \
-            else math.atan2(y, x)
-
-        v = rot_z(-q1).dot(v)
-        x = v[0] - self.t1[0]
-        y = v[1]
-        z = v[2] - self._mount_t[2] - self.t1[2]
-
-        q2 = self.ma2
-        q2 += (math.pi/2)*np.sign(z) if x == 0 \
-            else math.atan2(z, x)
-
-        q2 += np.arccos((l2**2 + x**2 + z**2 - l3**2) /
-                        (2*l2*np.sqrt(x**2+z**2)))
-
-        q3 = -self.ma3
-        q3 += np.arccos((l2**2 + l3**2 - x**2 - z**2) /
-                         (2*l2*l3)) - math.pi
-
-        return q1, q2, q3
+        return np.array([q1, q2, q3])
