4ea2637eba1bcb6c54feb12db4a5cec704a5261d
Piotr Patek <piotrpatek17@gmail.com>
2024-02-10 02:19:30 +0100

    #5 Added tests for KinematicsSolver initialization, added conversion from and to degres for forward and inverse kinematics solvers

--- kinematics/kinematics_solvers.py
+++ kinematics/kinematics_solvers.py
@@ -1,6 +1,4 @@
 import numpy as np
-import math
-from typing import Tuple
 from kinematics.kinematics_utils import homogeneous_transform_matrix, rot_z, rot_x
 
 
@@ -11,44 +9,81 @@ class KinematicsSolver:
     The leg has 3 degrees of freedom.
     """
 
-    def __init__(self, mount_t: np.ndarray, t1: np.ndarray, t2: np.ndarray, t3: np.ndarray):
+    def __init__(self, m1: np.ndarray, a1: np.ndarray, a2: np.ndarray, a3: np.ndarray):
         """
-        @param mount_t: Translation from world origin to J1
-        @param t1: Translation 1 (between J1 and J2)
-        @param t2: Translation 2 (between J2 and J3)
-        @param t3: Translation 3 (between J3 and FCP)
+        @param m1: Translation from world origin to J1
+        @param a1: Link 1 (between J1 and J2)
+        @param a2: Link 2 (between J2 and J3)
+        @param a3: Link 3 (between J3 and FCP)
         """
 
-        assert mount_t.ndim == 1
-        assert t1.ndim == 1
-        assert t2.ndim == 1
-        assert t3.ndim == 1
-        assert len(mount_t) == 3
-        assert len(t1) == 3
-        assert len(t2) == 3
-        assert len(t3) == 3
+        self.m1 = m1
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        
+    @property
+    def a1(self) -> np.ndarray:
+        return self._a1
+    
+    @a1.setter
+    def a1(self, new_a1: np.ndarray) -> None:
+        assert new_a1.ndim == 1
+        assert len(new_a1) == 3
+        
+        self._a1 = new_a1
 
-        self._mount_t = mount_t
-        self._t1 = t1
-        self._t2 = t2
-        self._t3 = t3
+    @property
+    def a2(self) -> np.ndarray:
+        return self._a2
+
+    @a2.setter
+    def a2(self, new_a2: np.ndarray) -> None:
+        assert new_a2.ndim == 1
+        assert len(new_a2) == 3
+
+        self._a2 = new_a2
+
+    @property
+    def a3(self) -> np.ndarray:
+        return self._a3
+
+    @a3.setter
+    def a3(self, new_a3: np.ndarray) -> None:
+        assert new_a3.ndim == 1
+        assert len(new_a3) == 3
+
+        self._a3 = new_a3
+
+    @property
+    def m1(self) -> np.ndarray:
+        return self._m1
+
+    @m1.setter
+    def m1(self, new_m1: np.ndarray) -> None:
+        assert new_m1.ndim == 1
+        assert len(new_m1) == 3
+
+        self._m1 = new_m1
 
     def forward(self, q: np.ndarray) -> np.ndarray:
         """
         Forward Kinematics
 
-        @param q: Vector of joint variables [q1,q2,q3]
+        @param q: Vector of joint angles [q1,q2,q3] in degrees
         @return: Position of leg's foot center point as [x,y,z]
         """
-        rot_0_1 = rot_z(q[0])
-        rot_1_2 = rot_x(np.pi/2)@rot_z(q[1])
-        rot_2_3 = rot_z(q[2])
+        q_rad = [np.deg2rad(x) for x in q]
+        
+        rot_0_1 = rot_z(q_rad[0])
+        rot_1_2 = rot_x(np.pi/2) @ rot_z(q_rad[1])
+        rot_2_3 = rot_z(q_rad[2])
         rot_3_4 = rot_z(0)
 
-        trans_0_1 = np.array([0, 0, self._mount_t[2]])
-        trans_1_2 = self._t1
-        trans_2_3 = self._t2
-        trans_3_4 = self._t3
+        trans_0_1 = np.array([0, 0, self._m1[2]])
+        trans_1_2 = self._a1
+        trans_2_3 = self._a2
+        trans_3_4 = self._a3
 
         t_0_1 = homogeneous_transform_matrix(rot_0_1, trans_0_1)
         t_1_2 = homogeneous_transform_matrix(rot_1_2, trans_1_2)
@@ -75,10 +110,10 @@ class KinematicsSolver:
         z = p[2]
 
         q1 = np.arctan2(y, x)
-        l = np.sqrt(x ** 2 + y ** 2) - self._t1[0]
-        z = z - self._mount_t[2] - self._t1[2]
+        l = np.sqrt(x ** 2 + y ** 2) - self._a1[0]
+        z = z - self._m1[2] - self._a1[2]
 
-        q2 = np.arctan2(z, l) + np.arccos((l ** 2 + self._t2[0] ** 2 - self._t3[0] ** 2) / (2 * l * self._t2[0]))
-        q3 = np.arccos((self._t2[0] ** 2 + self._t3[0] ** 2 - l ** 2) / (2 * self._t2[0] * self._t3[0])) - np.pi
+        q2 = np.arctan2(z, l) + np.arccos((l ** 2 + self._a2[0] ** 2 - self._a3[0] ** 2) / (2 * l * self._a2[0]))
+        q3 = np.arccos((self._a2[0] ** 2 + self._a3[0] ** 2 - l ** 2) / (2 * self._a2[0] * self._a3[0])) - np.pi
 
-        return np.array([q1, q2, q3])
+        return np.array([np.rad2deg(x) for x in [q1, q2, q3]])
---
+++ kinematics/tests/test_kinematics_initialization.py
@@ -0,0 +1,66 @@
+from pytest import raises
+from kinematics.kinematics_solvers import KinematicsSolver
+import numpy as np
+from numpy.testing import assert_allclose
+
+ATOL = 1e-8
+RTOL = 1e-8
+
+
+def test_kinematics_proper_initialization():
+    m1 = np.array([0, 0, 0.05])
+    a1 = np.array([0.05, 0, -0.035])
+    a2 = np.array([0.09, 0, 0])
+    a3 = np.array([0.1, 0, 0])
+
+    kinematics_solver = KinematicsSolver(m1, a1, a2, a3)
+
+    assert_allclose(m1, kinematics_solver.m1, rtol=RTOL, atol=ATOL)
+    assert_allclose(a1, kinematics_solver.a1, rtol=RTOL, atol=ATOL)
+    assert_allclose(a2, kinematics_solver.a2, rtol=RTOL, atol=ATOL)
+    assert_allclose(a3, kinematics_solver.a3, rtol=RTOL, atol=ATOL)
+
+
+def test_kinematics_improper_m1_initialization():
+    m1 = np.array([0, 0, 0.05, 1])
+    a1 = np.array([0.05, 0, -0.035])
+    a2 = np.array([0.09, 0, 0])
+    a3 = np.array([0.1, 0, 0])
+
+    with raises(AssertionError):
+        kinematics_solver = KinematicsSolver(m1, a1, a2, a3)
+
+
+def test_kinematics_improper_a1_initialization():
+    m1 = np.array([0, 0, 0.05])
+    a1 = np.array([0.05, 0, -0.035, 1])
+    a2 = np.array([0.09, 0, 0])
+    a3 = np.array([0.1, 0, 0])
+
+    with raises(AssertionError):
+        kinematics_solver = KinematicsSolver(m1, a1, a2, a3)
+
+
+def test_kinematics_improper_a2_initialization():
+    m1 = np.array([0, 0, 0.05])
+    a1 = np.array([0.05, 0, -0.035])
+    a2 = np.array([0.09, 0, 0, 1])
+    a3 = np.array([0.1, 0, 0])
+
+    with raises(AssertionError):
+        kinematics_solver = KinematicsSolver(m1, a1, a2, a3)
+
+
+def test_kinematics_improper_a3_initialization():
+    m1 = np.array([0, 0, 0.05])
+    a1 = np.array([0.05, 0, -0.035])
+    a2 = np.array([0.09, 0, 0])
+    a3 = np.array([0.1, 0, 0, 1])
+
+    with raises(AssertionError):
+        kinematics_solver = KinematicsSolver(m1, a1, a2, a3)
+
+
+
+
+
