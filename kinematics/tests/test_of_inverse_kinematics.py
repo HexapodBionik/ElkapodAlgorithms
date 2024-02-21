e8fc6a1a8806448038a45087cbcc40c0ab8c98fc
barszot <barszott@gmail.com>
2024-02-21 23:09:30 +0100

    #5 quick fix

--- kinematics/tests/test_of_inverse_kinematics.py
+++ kinematics/tests/test_of_inverse_kinematics.py
@@ -42,6 +42,5 @@ def test_3():
     a2 = np.array([1, 0, 0])
     a3 = np.array([1, 0, 0])
     leg = KinematicsSolver(m1, a1, a2, a3)
-    res = leg.inverse(np.array([0,0,-1]))
     assert_allclose(leg.inverse(np.array([0, 0, -1]))[1:], np.array([-90, -90]), rtol=RTOL, atol=ATOL)
 
